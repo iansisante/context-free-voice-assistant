c5400431259cb46de95d554a76a80a78be7110a5
iansisante <iansisante@gmail.com>
2022-05-10 17:08:57 +0800

    news index error

---
+++ news.py
@@ -0,0 +1,43 @@
+from tkinter import E
+from bs4 import BeautifulSoup as bs
+import requests
+
+# DEFAULT URL
+url = "https://www.google.com/search?q="
+
+def gamespot():
+
+    try:
+
+        # GET DATA FROM GAMESPOT
+        url = 'https://www.gamespot.com/news'
+        result = requests.get(url)
+        doc = bs(result.text, 'html.parser')
+
+        # FIND ALL TITLES
+        news = doc.find_all("h4", attrs={"class": "card-item__title"})
+
+        # INITIATION FOR SAVING WEBSCRAPED DATA
+        newsOut = []
+        i = 0
+
+        # SAVE ALL WEBSCRAPED DATA INTO DICT
+        for n in news:
+            print(i)
+            newsOut[i]['link'] = "https://www.gamespot.com" + n.parent['href']            
+            newsOut[i]['text'] = n.text
+
+            nDate = str(n.parent.next_sibling.time['datetime']).split(" ")
+            newsOut['date'] = nDate[1]+" "+nDate[2]+" "+nDate[3]
+
+            i+=1
+            #susume = input("Next? y/n: ")
+
+        
+        return newsOut
+
+    # CATCHES UNEXPECTED EXCEPTIONS
+    except Exception as e:
+        return "Something occured! Exception name: " + str(e)
+
+print(gamespot())
