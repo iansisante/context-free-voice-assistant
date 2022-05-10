3760aba4e3bfd24f30655ea6956be2bcb4f2a91f
iansisante <iansisante@gmail.com>
2022-05-10 16:05:32 +0800

    remove unused imports

--- search.py
+++ search.py
@@ -1,11 +1,7 @@
-from bs4 import BeautifulSoup as bs
-from more_itertools import first
-import requests
 import webbrowser
 
 google = "https://www.google.com/search?q="
 
 def websearch(search):
-    
     surl = google+search    
     webbrowser.get().open(surl)
