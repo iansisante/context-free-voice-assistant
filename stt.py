9174a61b69e3a238fe95e94536bb4d80eeae1258
mpbstrd <matthewperrybustarde@gmail.com>
2022-05-17 17:14:41 +0800

    added speech to text functionaility

--- stt.py
+++ stt.py
@@ -1,4 +1,31 @@
-import random 
+from playsound import playsound
+import speech_recognition as sr
+from gtts import gTTS
+import os
 
-r = random.randint(1,100)
-print(r)
+# Capture Voice
+# takes command through microphone
+def takecommand():
+	r = sr.Recognizer()
+	with sr.Microphone() as source:
+		print("listening.....")
+		r.pause_threshold = 1
+		audio = r.listen(source)
+
+	try:
+		print("Recognizing.....")
+		query = r.recognize_google(audio, language='en-in')
+		print(f"user said \n {query}\n")
+		return query
+	except Exception as e:
+		print("say that again please.....")
+		return "None"
+	return query
+
+# Taking voice input from the user
+# query returns the string output
+query = takecommand()
+while (query == "None"):
+	query = takecommand()
+
+
