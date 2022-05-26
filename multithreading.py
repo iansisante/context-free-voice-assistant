d223772f3ce70cd34fc58c60c4065e919b4d7843
iansisante <iansisante@gmail.com>
2022-05-26 17:29:03 +0800

    step-by-5 news reading

--- multithreading.py
+++ multithreading.py
@@ -1,45 +1,28 @@
-from concurrent.futures import thread
+from pyparsing import And
 import search
 from threading import *
 from news import gamespot as g_news
 import time
 
-the_value = ""
 datas = g_news()
+print(type(datas))
 
-# GET USER INPUT IF REDIRECT TO LINK
-def voiceRead():    
-    print("SAY YES IF WANT TO READ THE LINK!")
-    the_value = str(input("Voice input: "))
-    return the_value
+i = 0
+for data in datas:
+    print(data,'\n')
+    i+=1
 
-# SETS THE CURRENT DATA INTO A REDIRECTABLE DATA
-def getNews():
-    current_data = []
+    if(i%5==0):
+        ask = str(input("\nRead link? (Number or n) "))
+        try:
+            iAsk = int(ask)
 
-    for data in datas:
+            if(iAsk>0 and iAsk<6):
+                print("Selected: ", data,'\n')
 
-        current_data = data
-        print(current_data['link'])
-        print()
-
-        time.sleep(0.5)
-        return
-
-    return current_data
-    
-# FIRST CREATED THREAD IS THREAD 1
-t1 = Thread(target=getNews)
-t1.start()
-
-
-# THIS IS THE MAIN THREAD BUT NOT THREAD 1
-# MAIN THREAD RUNS WHILE THREAD 1 IS STILL RUNNING
-while(t1.is_alive()):
-    
-    if voiceRead() == "yes":
-        print(current_data)
-
+        except:
+            pass
         
-
-# print(search.websearch("what is the best genshin support"))
+        if(ask == 'n'):
+            print("ok boomer")
+        
